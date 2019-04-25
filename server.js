b2e9a0e49f813015dadedad1c7ac3b188672bf15
michelletm <michellemirek@gmail.com>
2019-04-25 10:02:41 -0700

    heroku issues

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-///NEW server.js for NYT Scraper/////
-
 var express = require("express");
 var mongojs = require("mongojs");
 var axios = require("axios");
@@ -30,15 +28,10 @@ var articlesRouter = require('./routes/articles');
 
 connection = require("./config/connection");
 
-// Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
 db.on("error", function(error) {
   console.log("Database Error:", error);
 });
-
-// Main route (simple Hello World Message)
-
-  
 
 
 app.use('/', indexRouter);
@@ -57,61 +50,18 @@ app.get("/test", async function(req, res){
 
 })
 
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// Retrieve data from the db
 app.get("/all", function(req, res) {
-  // Find all results from the scrapedData collection in the db
+  
   db.Article.find({}, function(error, data) {
-    // Throw any errors to the console
+
     if (error) {
       console.log(error);
     }
-    // If there are no errors, send the data to the browser as json
     else {
       res.json(found);
     }
   });
 });
-
-// // Scrape data from one site and place it into the mongodb db
-// app.get("/scrape", function(req, res) {
-//   // Make a request via axios for the news section of `ycombinator`
-//   axios.get("https://news.ycombinator.com/").then(function(response) {
-//     // Load the html body from axios into cheerio
-//     var $ = cheerio.load(response.data);
-//     // For each element with a "title" class
-//     $(".title").each(function(i, element) {
-//       // Save the text and href of each link enclosed in the current element
-//       var title = $(element).children("a").text();
-//       var link = $(element).children("a").attr("href");
-
-//       // If this found element had both a title and a link
-//       if (title && link) {
-//         // Insert the data in the scrapedData db
-//         db.scrapedData.insert({
-//           title: title,
-//           link: link
-//         },
-//         function(err, inserted) {
-//           if (err) {
-//             // Log the error if one is encountered during the query
-//             console.log(err);
-//           }
-//           else {
-//             // Otherwise, log the inserted data
-//             console.log(inserted);
-//           }
-//         });
-//       }
-//     });
-//   });
-
-//   // Send a "Scrape Complete" message to the browser
-//   res.send("Scrape Complete");
-// });
 
 
 app.listen(process.env.PORT || 3000, function() {
